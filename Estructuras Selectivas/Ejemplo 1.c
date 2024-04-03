1ff8d8eafd05454ed6788c2b5cb82e5981740b02
Ezequiel Federico <ezefenave2209@gmail.com>
2024-04-03 20:53:33 -0300

    asd

--- Estructuras Selectivas/Ejemplo 1.c
+++ Estructuras Selectivas/Ejemplo 1.c
@@ -1,16 +1,24 @@
+/* El siguiente programa realiza una conteo desde el primer numero ingresado hasta el segundo ingresado mostrando unicamente los numeros pares */
+
 #include <stdio.h>
 //While
 int main(){
-    int numero;
-
+    int numero1;
+    int numero2;
+    
     printf("Ingrese un numero: ");
-    scanf("%d",&numero);
+    scanf("%d",&numero1);
 
-    while (numero < 10)
-    {
-        numero++;
-        printf("%d ", numero);
+    printf("Ingrese el numero hasta el cual quiera iterar: ");
+    scanf("%d",&numero2);
+
+    while(numero1 < numero2) {
+        if(numero1%2 == 0){
+            printf("%d ", numero1);
+        }
+        numero1++;
     }
+
     printf("\n");
     printf("Fuera del while\n");
         return 0;
