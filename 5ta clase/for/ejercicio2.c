df160a987ad8158b816cf5557de46eca1c356455
Ezequiel Federico <ezefenave2209@gmail.com>
2024-04-10 20:57:17 -0300

    Commit

--- 5ta clase/for/ejercicio2.c
+++ 5ta clase/for/ejercicio2.c
@@ -3,13 +3,18 @@
 int main(){
     int numero = 0;
     int acumulador = 0;
+    int contador = 0;
+    int promedio;
 
     for (int i= 0; i < 5; i++){
         printf("Ingrese un numero: ");
         scanf("%d", &numero);
-        
+
         acumulador = acumulador + numero;
         printf("El valor incrementado es: %d\n", acumulador); 
+        contador++;
     }
+    promedio = acumulador/contador;
+    printf("El valor promedio de los numeros ingresados es de: %d\n", promedio); 
     return 0;
 }
