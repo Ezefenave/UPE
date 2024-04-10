f5f4bc29ad00a64cb1bde4c4be7763d0ad3ca763
Ezequiel Federico <ezefenave2209@gmail.com>
2024-04-10 20:24:58 -0300

    asd

--- 5ta clase/for/ejercicio2.c
+++ 5ta clase/for/ejercicio2.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    int numero;
-    int contador;
+    int numero = 0;
+    int contador = 0;
 
     for (int i= 0; i <5; i++){
         printf("Ingrese un numero: ");
         scanf("%d", &numero);
+
+        contador = numero + numero + numero + numero + numero;
     }
-    contador = numero + numero + numero + numero + numero;
+    
     printf("El numero es: %d", contador);
 
     return 0;
