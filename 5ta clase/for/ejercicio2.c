2196858ce06a6ee0e7cf6abd0f1fa0b6cc990107
Ezequiel Federico <ezefenave2209@gmail.com>
2024-04-10 21:17:49 -0300

    Fin quinta clase

--- 5ta clase/for/ejercicio2.c
+++ 5ta clase/for/ejercicio2.c
@@ -9,10 +9,15 @@ int main(){
     for (int i= 0; i < 5; i++){
         printf("Ingrese un numero: ");
         scanf("%d", &numero);
+        if(numero > 0 ){
 
         acumulador = acumulador + numero;
         printf("El valor incrementado es: %d\n", acumulador); 
         contador++;
+        }  
+        else{
+            printf("El valor ingresado es negativo, ingrese un numero valido\n");
+        }
     }
     promedio = acumulador/contador;
     printf("El valor promedio de los numeros ingresados es de: %d\n", promedio); 
