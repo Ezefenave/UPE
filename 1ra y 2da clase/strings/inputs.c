d718be70d92efcc5afaf419f22ae199d60caa79d
Ezequiel Federico <ezefenave2209@gmail.com>
2024-04-11 11:19:12 -0300

    Orden

--- 1ra y 2da clase/strings/inputs.c
+++ 1ra y 2da clase/strings/inputs.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
 int main(){
-    int primerNumero;
-    int segundoNumero;
+    float primerNumero;
+    float segundoNumero;
     int resultadoSuma;
     int restultadoMultiplicacion;
     char caracterIngresado;
 
     printf("Ingrese un numero: ");
-    scanf ("%.2f"",&primerNumero);
+    scanf ("%f",&primerNumero);
                                             //De esta manera puedo solicitar numeros
     printf("Ingrese otro numero: ");
-    scanf ("%.2f"",&segundoNumero);
+    scanf ("%f",&segundoNumero);
 
     resultadoSuma = primerNumero + segundoNumero; 
 
-    printf("El resultado de la suma de esos numeros es: %d ", resultadoSuma); 
+    printf("El resultado de la suma de esos numeros es: %d\n", resultadoSuma); 
 
     restultadoMultiplicacion = primerNumero * segundoNumero;
 
-    printf("El resultado de la multiplicacion de esos numeros es: %d ", restultadoMultiplicacion);  
+    printf("El resultado de la multiplicacion de esos numeros es: %d\n ", restultadoMultiplicacion);  
 
     fflush(stdin);
 
     printf("Ingrese un caracter: ");
-    scanf(" %c", &caracterIngresado);       //De esta manera puedo solicitar caracteres
-    printf("El caracter ingresado es: %c ", caracterIngresado);
+    scanf("%c", &caracterIngresado);       //De esta manera puedo solicitar caracteres
+    printf("El caracter ingresado es: %c\n", caracterIngresado);
 
     return 0;
 
