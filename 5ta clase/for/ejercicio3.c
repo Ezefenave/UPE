fc10d9f8309c9d37cae99cc9ac42b6265327a5f7
Ezequiel Federico <ezefenave2209@gmail.com>
2024-04-10 22:48:54 -0300

    Fin clase 5

--- 5ta clase/for/ejercicio3.c
+++ 5ta clase/for/ejercicio3.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 
 int main(){
-    int contador = 0;
+    int contador_mayores = 0;
     int edad;
+    int caja = 0;
 
-    
+    printf("Bienvenido al museo \n");
 
     do{
-        printf("Ingrese su edad:" );
+        printf("\nIngrese la edad de la persona que quiere ingresar al museo (Ingrese 0 para salir): ");
         scanf("%d", &edad);
-        if (edad >= 18){
-            contador++;
+
+        if (edad >= 18 && edad <= 100){
+            contador_mayores++; 
+            caja = caja + 150;
+            printf("--------------------------------------------------------------\n");
+            printf("La persona que ingreso es mayor y se le cobraron $150\n");
+            printf("--------------------------------------------------------------\n");
+        }
+        else if(edad > 0){
+            printf("--------------------------------------------------------------\n");
+            printf("La persona que ingreso es menor y se le cobraron $50\n");
+            printf("--------------------------------------------------------------\n");
+            caja = caja + 50;
         }
     }
-    while (edad != -1 );
-    printf("La cantidad de personas mayores de edad es: %d", contador);
-    
+    while (edad != 0);
+    printf("--------------------------------------------------------------\n");
+    printf("\nEl pogama finalizo correctamente \n");
+    printf("\n--------------------------------------------------------------");
+    printf("\nLa cantidad de personas mayores de edad que ingresaron al museo es de: %d\n", contador_mayores);
+    printf("La caja actual es de: $%d\n", caja);
+    printf("\n--------------------------------------------------------------");
 
     return 0;
-
 }
