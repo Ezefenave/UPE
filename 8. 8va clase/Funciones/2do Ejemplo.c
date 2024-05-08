be91334a827675b63df4730f6d64059dbe5653ff
Ezequiel Federico <ezefenave2209@gmail.com>
2024-05-08 19:36:59 -0300

    Orden

--- 8. 8va clase/Funciones/2do Ejemplo.c
+++ 8. 8va clase/Funciones/2do Ejemplo.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
+int sumar(int, int);
 int sumarV2();  //Prototipo de la función, se debe declarar antes  de declarar la función, en esta se indican los tipos de datos que trabaja mi función
 
 int main(){
+
     int totalSuma;
-
     totalSuma = sumarV2();
+    printf("La suma de los numeros es: %d\n", totalSuma);    
 
-    printf("La suma de los numeros es: %d\n", totalSuma);
-    
     return 0;
 }
 
@@ -22,6 +22,12 @@ int sumarV2(int numero1, int numero2){ //Se deben colocar los parametros entre p
     scanf("%d",&numero2);
 
     totalSuma = numero1 + numero2;
-    
     return totalSuma;
+}
+
+int sumar(int valor1, int valor2){ //Se deben colocar los parametros entre parentesis
+    int total;
+    total = valor1 + valor2;
+    
+    return total;
 }
