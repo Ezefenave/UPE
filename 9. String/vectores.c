b3de5b1d6944fd87acd26b318f38f300e3b85931
ezefenave <ezefenave2209@gmail.com>
2024-05-14 11:49:54 -0300

    Orden

--- 9. String/vectores.c
+++ 9. String/vectores.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main(){
-    
+#define tamaio 10
+#define tam 3
+
+int main(void){
+
+    int numero = 0;
+    int num;
+
+    int vectorNumeros [3];
+
+    for (int i =0; i<3; i++){
+        printf("Ingrese un numero para guardar en el vector: ");
+        scanf("%d",&vectorNumeros[i]);
+    }
+
+     for (int i =0; i<3; i++){
+        printf("Numero dentro del vector: ");
+        scanf("%d",&vectorNumeros[i]);
+     }
+
+     printf("El valor en el indice 2 es: %d", vectorNumeros[2]);
+
     return 0;
 }
