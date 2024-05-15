85f657a7f1adc42fb86595d34199c3daf57eeaee
ezefenave <ezefenave2209@gmail.com>
2024-05-15 20:42:36 -0300

    Orden

--- 9. vectores/explicacion2.c
+++ 9. vectores/explicacion2.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    char primeraManera[9] = "Ezequiel";
-    char segundaManera[9];
+    char nombre[15];
+    char apellido[15];
 
+    printf("Ingrese su nombre: ");
+    fgets(nombre, sizeof(nombre), stdin);
+
+    for (int i = 0; nombre[i] != '\0'; i++){ //Inicio un bucle que recorra el vector hasta encontrar el '\0' que me sirve para darle indice a los caracteres dentro de mi funcion
+        if (nombre[i] == '\n'){ 
+            nombre[i] = '\0'; //Busco el indice donde se almaceno el salto de linea y lo remplazo por el '\0'
+        }
+    }
+
+    printf("Ingrese su apellido: ");
+    fgets(apellido, sizeof(apellido), stdin);
+
+    printf("Su nombre y apellido son: %s %s",nombre, apellido);
 }
