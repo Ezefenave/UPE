0e18b160fad4326eff446931ef5441ff2ffbfb05
ezefenave <ezefenave2209@gmail.com>
2024-05-15 19:51:35 -0300

    Orden

--- 9. vectores/ejercicio2.c
+++ 9. vectores/ejercicio2.c
@@ -4,15 +4,16 @@
 int main() {
     int vectorNumeros[tam];
     int acumulador = 0;
-    int promedio = 0;
+    float promedio = 0;
 
     for(int i = 0; i < tam; i++) {
         printf("Ingrese el numero que desea agregar al vector: "); 
         scanf("%d", &vectorNumeros[i]); 
         acumulador += vectorNumeros[i]; //De esta manera acumulo los numeros que me ingresan al vector
     }
-    
-    promedio = acumulador / tam;
-    printf("El promedio de los numeros que se encuentran dentro del vector es:%d ", promedio); 
+
+    // Casteo se debe poner entre parentesis el tipo de variable que quiero darle a la funcion
+    promedio = (float)acumulador / tam;
+    printf("El promedio de los numeros que se encuentran dentro del vector es: %.1f", promedio); 
     return 0;
 }
