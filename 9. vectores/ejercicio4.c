0270ca0f057f89d5621dbd6d70534fc33ae8b1b7
ezefenave <ezefenave2209@gmail.com>
2024-05-15 23:26:18 -0300

    Orden

--- 9. vectores/ejercicio4.c
+++ 9. vectores/ejercicio4.c
@@ -12,6 +12,7 @@ int main() {
     int banderaCompletado = 0;
     int longitud;
     char confirmacion;
+    int banderaPista = 0;
     
     printf("Bienvenido al juego\n");
 
@@ -31,24 +32,25 @@ int main() {
         } else { //En caso de que no sea correcta le indico al usuario y disminuyo un intento
             limiteIntentos--; 
             printf("Intento fallido. Te quedan %d intentos\n", limiteIntentos); //Le muestro al usuario la cantidad de intentos restantes
+            
+        if(banderaPista == 0){
             printf("Desea obtener una pista?. Ingrese s para obtener una pista: ");
             scanf("%c", &confirmacion);
-            getchar(); //
-
-            if(confirmacion == 's') {
+            getchar(); //Con el getchar logro que solo
+        }
+            if(confirmacion == 's' ) { //Coloco una bandera para que pueda mostrarle esto al usuario una sola vez
             longitud = strlen(palabraSecreta);
+            printf("-------------------------------------------------------------------------------------\n");
             printf("La longitud de la palabra secreta es de: %d letras\n", longitud);
             printf("La primera letra de la palabra secreta es: '%c'\n", palabraSecreta[0]); //Muestro la primera letra de la palabra secreta 
             printf("La ultima letra de la palabra secreta es: '%c'\n", palabraSecreta[strlen(palabraSecreta) - 1]); //Muestro la ultima letra de la palabra secreta
             printf("-------------------------------------------------------------------------------------\n");
+            banderaPista = 1;
         }
-            
         }
         
     }
     while (limiteIntentos > 0 && banderaCompletado == 0); //Verifico si el usuario tiene intentos restantes ademas de verificas si el usuario no adivino al primer inteno
-
-    
 
     if (banderaCompletado == 0) {
         printf("\nHas agotado tus intentos. La palabra secreta era: %s\n", palabraSecreta); //Si al finalizar el bucle el usuario no adivino, por lo que no cambia la bandera imprimo el mensaje que le indica al usuario la palabra oculta
